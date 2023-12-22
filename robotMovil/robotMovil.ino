e691b808ff4296f01028412042f9b722543c711c
marcoaspire <ocrampac@gmail.com>
2023-12-22 22:17:14 +0100

    avances, para velocidad angular y lineal

--- robotMovil/robotMovil.ino
+++ robotMovil/robotMovil.ino
@@ -11,10 +11,24 @@
 #include <WiFi.h>
 #include <MQTT.h>
 
-
 WiFiClient net;
 MQTTClient client;
+unsigned long tiempoInicio;
+float velocidadIzquierda;
+float velocidadDerecha;
+float velocidadAngular; 
+float velocidadLineal; 
+float velocidadSegundos;
 
+unsigned long tiempoTranscurrido;
+
+int b = 16;
+  int x = 0;
+  int y = 0;
+  int theta = 0;
+
+    AccelStepper motorIzquierdo(AccelStepper::DRIVER, STP1);
+    AccelStepper motorDerecho(AccelStepper::DRIVER, STP2);
 unsigned long lastMillis = 0;
 // int velocidad = 2500;
 
@@ -32,14 +46,21 @@ public:
     Rueda mide 8 cm, perimetro pi * diametro = 25.13 cm avanza cada vuelta es esta
     25 cm - necesita 3200 pulsos aka 360 
     i.e 8.888 pulsos equivale a un grado  
-    grados | pulsos
-      180  | 3200 vuelta
-       90 | 1600
-       45  | 800
-       22.5  | 400  
+    distincia | grados | pulsos
+    25.13  |360 | 3200 | 8 s
+    12.065 |180 | 1600 | 4 seg
+    6.03   |90  | 800  | 2
+    3.01   |45  | 400  | 1
+    1.505  |22.5| 200  | .5
+    .0335  |1   | 8.888| 
+           |.05  |  1
     velocidad angular = angulo rotado/ tiempo 
+    velocidad lineal = distancia / tiempo
+
   */ 
   Coche() {
+
+
     pinMode(STP1, OUTPUT);
     pinMode(STP2, OUTPUT);
     pinMode(EN, OUTPUT);
@@ -82,6 +103,10 @@ public:
     digitalWrite( STP1, LOW );
     //delayMicroseconds(500);
     delayMicroseconds(velocidad); //2
+    //velocidadIzquierda = motorIzquierdo.speed();
+    //velocidadDerecha = motorDerecho.speed();
+    //Serial.println("velocidadDer: " + String(velocidadDerecha));
+    //Serial.println("velocidadIzq: " + String(velocidadIzquierda));
   }
 
   void vueltaIzquierda(){
@@ -174,10 +199,28 @@ void connect() {
   client.subscribe("comandos");
 }
 
+// void multiplicarMatriz(float matriz [][2]){
+//   int res [][2];
+//   for (int i=0; i<matriz.length(); i++)
+//     for (int j=0; j<matriz[0].length();j++)
+//       res[i][j]= matriz[i][j]  ; 
 
+// }
 
+void actualizar_posicion(float matriz_transformacion [][2]){
+  int vel_lineal = matriz_transformacion[0][0] *velocidadIzquierda + matriz_transformacion[0][1] * velocidadDerecha;
+  int vel_angular = matriz_transformacion[1][0]*velocidadIzquierda + matriz_transformacion[1][1] * velocidadDerecha;
+  // actualiza 
+  x += vel_lineal * cos(theta);
+  y += vel_lineal * sin(theta);
+  theta += vel_angular;
+
+}
 
 void setup() {
+
+  float matriz_transformacion[2][2]={{1/2,1/2},{-1/2*b,1/2*b}};
+
   Serial.begin(115200);
 
   // start wifi and mqtt
@@ -222,10 +265,19 @@ void frenado(int velocidadRecibida)
 
 void delantePulso()
 {
+  tiempoInicio = micros();
   int pu = 0;
   // 12 cm
   while (pu < 1601) {
     myCoche.avanzaVuelta();
+
+    // no
+    // velocidadIzquierda = motorIzquierdo.speed();
+    // if (velocidadIzquierda != 0)
+    //   Serial.println("2velocidadIzq: " + String(velocidadIzquierda));
+    // else
+    //   Serial.print("@");
+
     //myCoche.vueltaIzquierda();
     //delay(10); // 10 000 //1
     //delayMicroseconds(velocidad); //2
@@ -234,22 +286,68 @@ void delantePulso()
     // delayMicroseconds(500); //3
     pu++;
   } 
+
+  // Medir el tiempo transcurrido
+  tiempoTranscurrido = micros() - tiempoInicio;
+
+  // 1M micros = 1 s
+  // x miros
+
+
+  // Calcular la velocidad en PPS, 1,000,000 (microsegundos en un segundo)
+  velocidadSegundos =  tiempoTranscurrido/ 1000000.0;
+  velocidadAngular = pu / velocidadSegundos;
+  //Tiempo
+  Serial.print("Tiempo: ");
+  Serial.println(velocidadSegundos);
+
+  // Mostrar la velocidad en el puerto serie
+  Serial.print("Velocidad angular actual: ");
+  Serial.println(velocidadAngular);
+
+  velocidadLineal = 12.065/ velocidadSegundos;  // cm por seg, esta es velocidad lineal de los motores
+  Serial.print("Velocidad lineal actual: ");
+  Serial.println(velocidadLineal);
+
+
+  // Reiniciar el tiempo para la siguiente medición
+  tiempoInicio = micros();
 }
 
 void giroIzquierdo()
 {
+  tiempoInicio = micros();
   int q = 0;
   /* 
     grados | pulsos
       180  | 3200 vuelta
-       90 | 1600
+       90  | 1600
        45  | 800
-       22.5  | 400  
+       22.5| 400  
   */
   while (q < 400) {
     myCoche.vueltaIzquierda();
     q++;
   }
+  // Medir el tiempo transcurrido
+  tiempoTranscurrido = micros() - tiempoInicio;
+
+  //Conociendo el angulo, la trayectoria = radio por angulo en radianes
+  // 180 = pi radianes
+  float anguloRadianes = 22.5*3.1416/180;
+  float distancidaRecorrida = 4*anguloRadianes;
+  Serial.print("Distancia recorrida vuelta izq cm: ");
+  Serial.println(distanciaRecorrida);
+
+  // Calcular la velocidad en PPS, 1,000,000 (microsegundos en un segundo)
+  velocidadSegundos =  tiempoTranscurrido/ 1000000.0;
+  velocidadLineal = distanciaRecorrida/velocidadSegundos;
+
+  Serial.print("Velocidad lineal actual: ");
+  Serial.println(velocidadLineal);
+
+   // Reiniciar el tiempo para la siguiente medición
+  tiempoInicio = micros();
 }
 
 void giroIzquierdoAtras()
@@ -285,6 +383,15 @@ void giroDerechoAtras()
 
 int qwerty = 0;
 void loop() {
+    //Serial.println("1velocidadDer: " + String(velocidadDerecha));
+
+    // no funciono
+    // velocidadIzquierda = motorIzquierdo.speed();
+    // if (velocidadIzquierda != 0)
+    //   Serial.println("2velocidadIzq: " + String(velocidadIzquierda));
+    // else
+    //   Serial.print("@");
+
   client.loop();
   delay(10);
 
